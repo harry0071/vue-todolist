dfd02f98fc9021badff71fe02dead0e74dc7851f
stage <1311012326@qq.com>
2018-10-10 14:30:22 +0800

    初步完成

--- src/main.js
+++ src/main.js
@@ -1,17 +1,107 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'css/style.css';
+
 import Vue from 'vue';
-import App from './App';
-import router from './router';
 
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
-new Vue({
+
+window.addEventListener('hashchange', function () {
+	let hashName = location.hash;
+	app.hashName = hashName;
+})
+
+const app = new Vue({
 	el: '#app',
-	router,
-	components: {
-		App
+	data:{
+		inputVal: '',
+		todoList: [],
+		editTodo: null,
+		oldTodo: null,
+		hashName: '#/',
 	},
-	template: '<App/>'
+	computed:{
+		remain(){//剩余
+			return this.todoList.filter(todo => !todo.completed).length; //返回completed为false的所有项
+		},
+
+		isAll:{
+			get(){
+				return this.remain === 0;//当剩余0项待办时，设置checkbox为true
+			},
+			set(newIsAll){
+				this.todoList.forEach( (todo, index) => {
+					//将所有todo的completed与isAll保持一致
+					todo.completed = newIsAll;
+				});
+			}
+		},
+
+		todoListFiltered(){
+			if (this.hashName === '#/') {
+				return this.todoList;
+			} else if (this.hashName === '#/active') {
+				return this.todoList.filter(todo => !todo.completed);
+			} else {
+				return this.todoList.filter(todo => todo.completed);
+			}
+		},
+	},
+	directives:{
+		focus(el,binding){
+			if (binding.value) {
+				el.focus();
+			}
+		}
+	},
+	methods:{
+		addTodo(){
+			if (!this.inputVal) {
+				alert('请输入内容！');
+			} else {
+				this.todoList.push({
+					id:Math.random(),
+					content:this.inputVal,
+					completed:false
+				});
+				this.inputVal = '';
+				}
+		},
+
+		deleteTodo(i){
+			this.todoList.splice(i,1);
+		},
+
+		handleEditTodo(todo){
+			this.oldTodo = JSON.parse(JSON.stringify(todo)); //cancleEdit取消编辑时，恢复成oldTodo
+			this.editTodo = todo;
+		},
+
+		commitEdit(todo,i){
+			//离开edit模式
+			this.editTodo = null;
+
+			//如果编辑清空，则删除该项
+			if(!todo.content){
+				this.deleteTodo(i);
+			}
+		},
+
+		cancleEdit(todo,i){
+			//离开edit模式
+			this.editTodo = null;
+
+			todo.content = this.oldTodo.content;
+		},
+
+		clearCompletedTodo(){
+			this.todoList = this.todoList.filter(todo => !todo.completed);
+		},
+	},
+})
+
+
+//管理另一个vue实例
+new Vue({
+	el: '#footer',
 })
